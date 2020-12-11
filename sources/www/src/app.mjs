856398fce036065e1d15c5d32e5a8bef7e3b62b0
Emrudia <elisabarre@orange.fr>
2020-12-11 10:31:12 +0100

    socket.io

--- sources/www/src/app.mjs
+++ sources/www/src/app.mjs
@@ -49,18 +49,18 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use('/my_webapp_node',express.static(__dirname + '/public'));
 
 //routes
-app.use('/api/projects',projectRoutes);
-app.use('/api/users',usersRoutes);
-app.use('/api/users/login',usersRoutes);
-app.use('/home',homeRoutes);
-app.use('/php',phpRoutes);
-app.use('/save',saveRoutes);
-app.use('/repos',gitRoutes);
-app.use('/slack',slackRoutes);
-app.use('/sms',smsRoutes);
-app.use('/mail',mailRoutes);
-app.use('/mastodon',mastodonRoutes);
-app.use('/zip',zipRoutes);
+app.use('/my_webapp_node/api/projects',projectRoutes);
+app.use('/my_webapp_node/api/users',usersRoutes);
+app.use('/my_webapp_node/api/users/login',usersRoutes);
+app.use('/my_webapp_node/home',homeRoutes);
+app.use('/my_webapp_node/php',phpRoutes);
+app.use('/my_webapp_node/save',saveRoutes);
+app.use('/my_webapp_node/repos',gitRoutes);
+app.use('/my_webapp_node/slack',slackRoutes);
+app.use('/my_webapp_node/sms',smsRoutes);
+app.use('/my_webapp_node/mail',mailRoutes);
+app.use('/my_webapp_node/mastodon',mastodonRoutes);
+app.use('/my_webapp_node/zip',zipRoutes);
 
 
 
